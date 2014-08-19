35f6ff96eb4a973a4a9f26bef06f2ded76dddb8f
jagspage2013 <jagspage2012@gmail.com>
2014-08-19 13:38:42 -0500

    first Commit

---
+++ app/src/main/java/mx/unam/saic/puntoycoma/controladores/MainActivity.java
@@ -0,0 +1,31 @@
+package mx.unam.saic.puntoycoma.controladores;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+import mx.unam.saic.puntoycoma.R;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+
+        }
+    }
+
+
+
+
+}
