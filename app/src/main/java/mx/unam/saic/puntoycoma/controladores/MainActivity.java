61da08e0e394900f87b333b8074a5df98a277291
jagspage2013 <jagspage2012@gmail.com>
2014-08-27 18:10:02 -0500

    Arreglo de Facebook

--- app/src/main/java/mx/unam/saic/puntoycoma/controladores/MainActivity.java
+++ app/src/main/java/mx/unam/saic/puntoycoma/controladores/MainActivity.java
@@ -1,68 +1,31 @@
 package mx.unam.saic.puntoycoma.controladores;
 
-import mx.unam.saic.puntoycoma.R;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.app.AlertDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.text.TextUtils;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
-
-
-import com.facebook.*;
-import com.facebook.model.GraphObject;
-import com.facebook.model.GraphPlace;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
-import com.facebook.widget.*;
+import com.facebook.widget.FacebookDialog;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.ProfilePictureView;
 
-import java.util.*;
+import mx.unam.saic.puntoycoma.R;
+
 
 public class MainActivity extends ActionBarActivity {
 
-	
-	
-private static final String PERMISSION = "publish_actions";
-    
-    //ManejoDatos data = new ManejoDatos();
-
-    private final String PENDING_ACTION_BUNDLE_KEY = "com.example.prueba:PendingAction";
-
-    //private Button postStatusUpdateButton;
-    //private Button exit;
     private LoginButton loginButton;
     private ProfilePictureView profilePictureView;
     private TextView greeting;
-    private PendingAction pendingAction = PendingAction.NONE;
     //private ViewGroup controlsContainer;
-    private GraphUser user;
-    private GraphPlace place;
-    private List<GraphUser> tags;
-    private boolean canPresentShareDialog;
-    private boolean canPresentShareDialogWithPhotos;
-
-    private enum PendingAction {
-        NONE,
-        POST_PHOTO,
-        POST_STATUS_UPDATE
-    }
     private UiLifecycleHelper uiHelper;
-
     private Session.StatusCallback callback = new Session.StatusCallback() {
         @Override
         public void call(Session session, SessionState state, Exception exception) {
@@ -70,92 +33,14 @@ private static final String PERMISSION = "publish_actions";
         }
     };
 
-    private FacebookDialog.Callback dialogCallback = new FacebookDialog.Callback() {
-        @Override
-        public void onError(FacebookDialog.PendingCall pendingCall, Exception error, Bundle data) {
-            Log.d("HelloFacebook", String.format("Error: %s", error.toString()));
-        }
-
-        @Override
-        public void onComplete(FacebookDialog.PendingCall pendingCall, Bundle data) {
-            Log.d("HelloFacebook", "Success!");
-        }
-    };
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         uiHelper = new UiLifecycleHelper(this, callback);
         uiHelper.onCreate(savedInstanceState);
-
-        if (savedInstanceState != null) {
-            String name = savedInstanceState.getString(PENDING_ACTION_BUNDLE_KEY);
-            pendingAction = PendingAction.valueOf(name);
-        }
-
-        setContentView(R.layout.activity_main);
-
         loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
-            @Override
-            public void onUserInfoFetched(GraphUser user) {
-                MainActivity.this.user = user;
-                
-                updateUI();
-              
-                handlePendingAction();
-            }
-        });
-
-        profilePictureView = (ProfilePictureView) findViewById(R.id.profilePicture);
-        greeting = (TextView) findViewById(R.id.greeting);
-
-        //postStatusUpdateButton = (Button) findViewById(R.id.postStatusUpdateButton);
-        /*postStatusUpdateButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                onClickPostStatusUpdate();
-            }
-        });*/
-        
-        /*exit = (Button) findViewById(R.id.salir);
-        exit.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                finish();
-            }
-        });*/
-        
-        //contenedor de los fragments
-        /*controlsContainer = (ViewGroup) findViewById(R.id.main_ui_container);
-
-        final FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if (fragment != null) {
-            
-            controlsContainer.setVisibility(View.GONE);
-            if (fragment instanceof FriendPickerFragment) {
-                setFriendPickerListeners((FriendPickerFragment) fragment);
-            } else if (fragment instanceof PlacePickerFragment) {
-                setPlacePickerListeners((PlacePickerFragment) fragment);
-            }
-        }*/
-
-        
-        /*fm.addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
-            @Override
-            public void onBackStackChanged() {
-                if (fm.getBackStackEntryCount() == 0) {
-                    
-                    controlsContainer.setVisibility(View.VISIBLE);
-                }
-            }
-        });*/
-
-        
-        canPresentShareDialog = FacebookDialog.canPresentShareDialog(this,
-                FacebookDialog.ShareDialogFeature.SHARE_DIALOG);
-        
-        canPresentShareDialogWithPhotos = FacebookDialog.canPresentShareDialog(this,
-                FacebookDialog.ShareDialogFeature.PHOTOS);
+        loginButton.setReadPermissions("");
     }
 
     @Override
@@ -163,10 +48,6 @@ private static final String PERMISSION = "publish_actions";
         super.onResume();
         uiHelper.onResume();
 
-        
-        AppEventsLogger.activateApp(this);
-
-        updateUI();
     }
 
     @Override
@@ -174,13 +55,12 @@ private static final String PERMISSION = "publish_actions";
         super.onSaveInstanceState(outState);
         uiHelper.onSaveInstanceState(outState);
 
-        outState.putString(PENDING_ACTION_BUNDLE_KEY, pendingAction.name());
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        uiHelper.onActivityResult(requestCode, resultCode, data, dialogCallback);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -196,252 +76,31 @@ private static final String PERMISSION = "publish_actions";
     }
 
     private void onSessionStateChange(Session session, SessionState state, Exception exception) {
-        if (pendingAction != PendingAction.NONE &&
-                (exception instanceof FacebookOperationCanceledException ||
-                exception instanceof FacebookAuthorizationException)) {
-                new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("cancelar")
-                    .setMessage("Mensaje")
-                    .setPositiveButton("OK", null)
-                    .show();
-            pendingAction = PendingAction.NONE;
-        } else if (state == SessionState.OPENED_TOKEN_UPDATED) {
-            handlePendingAction();
-        }
-        updateUI();
-    }
-
-    private void updateUI() {
-        //este actua cuando se inicia sesion
-    	Session session = Session.getActiveSession();
-        boolean startActivity = (session != null && session.isOpened());
-
-        //postStatusUpdateButton.setEnabled(enableButtons || canPresentShareDialog);
-        
-        if (startActivity && user != null) {
-            profilePictureView.setProfileId(user.getId());
-            greeting.setText(getString(R.string.hello_user, user.getFirstName()));
-        } else {
-            profilePictureView.setProfileId(null);
-            greeting.setText(null);
+        if (session != null && state.isOpened()) {
+            Log.i("SAIC", "Logged in...");
+            makeARequest(session);
+        } else if (state.isClosed()) {
+            Log.i("SAIC", "Logged out...");
         }
     }
 
-    @SuppressWarnings("incomplete-switch")
-    private void handlePendingAction() {
-        PendingAction previouslyPendingAction = pendingAction;
-        pendingAction = PendingAction.NONE;
+    private void makeARequest(final Session session) {
 
-        switch (previouslyPendingAction) {
-            case POST_PHOTO:
-                //postPhoto();
-                break;
-            case POST_STATUS_UPDATE:
-                //postStatusUpdate();
-                break;
-        }
-    }
+        Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
+            @Override
+            public void onCompleted(GraphUser user, Response response) {
+                if(session == Session.getActiveSession()){
+                    if(user!= null){
+                        Log.d("SAIC","EL USUARIO ES : "+ user.getFirstName());
 
-    private interface GraphObjectWithId extends GraphObject {
-        String getId();
-    }
-
-    private void showPublishResult(String message, GraphObject result, FacebookRequestError error) {
-        String title = null;
-        String alertMessage = null;
-        if (error == null) {
-            title = getString(R.string.success);
-            String id = result.cast(GraphObjectWithId.class).getId();
-            alertMessage = getString(R.string.successfully_posted_post, message, id);
-        } else {
-            title = getString(R.string.error);
-            alertMessage = error.getErrorMessage();
-        }
-
-        new AlertDialog.Builder(this)
-                .setTitle(title)
-                .setMessage(alertMessage)
-                .setPositiveButton("ok", null)
-                .show();
-    }
-
-    private void onClickPostStatusUpdate() {
-        performPublish(PendingAction.POST_STATUS_UPDATE, canPresentShareDialog);
-    }
-/*
-    private FacebookDialog.ShareDialogBuilder createShareDialogBuilderForLink() {
-        //aqui se manda el mensaje con imagen y una descripcion
-    	return new FacebookDialog.ShareDialogBuilder(this)
-                .setName("Nuevo Trámite realizado")
-                .setDescription("Hola acabo de trámitar "+data.setTramiteToFacebook(Conectar_FB.this)+", ¿tú ya lo hiciste?")
-                .setLink("http://google.com");
-    }
-
-    private void postStatusUpdate() {
-        if (canPresentShareDialog) {
-            FacebookDialog shareDialog = createShareDialogBuilderForLink().build();
-            uiHelper.trackPendingDialogCall(shareDialog.present());
-        } else if (user != null && hasPublishPermission()) {
-            //aqui se mandará el trámite que haces
-        	//
-        	//
-        	//
-        	//final String message = getString(R.string.status_update, user.getFirstName(), (new Date().toString()));
-        	final String message = "Hola acabo de trámitar "+data.setTramiteToFacebook(Conectar_FB.this)+", ¿tú ya lo hiciste?"+"  test3(final)";
-            Request request = Request
-                    .newStatusUpdateRequest(Session.getActiveSession(), message, place, tags, new Request.Callback() {
-                        @Override
-                        public void onCompleted(Response response) {
-                            showPublishResult(message, response.getGraphObject(), response.getError());
-                        }
-                    });
-            request.executeAsync();
-        } else {
-            pendingAction = PendingAction.POST_STATUS_UPDATE;
-        }
-    }*/
-
-    
-
-    private FacebookDialog.PhotoShareDialogBuilder createShareDialogBuilderForPhoto(Bitmap... photos) {
-        return new FacebookDialog.PhotoShareDialogBuilder(this)
-                .addPhotos(Arrays.asList(photos));
-    }
-
-    /*private void postPhoto() {
-        Bitmap image = BitmapFactory.decodeResource(this.getResources(), R.drawable.icon);
-        if (canPresentShareDialogWithPhotos) {
-            FacebookDialog shareDialog = createShareDialogBuilderForPhoto(image).build();
-            uiHelper.trackPendingDialogCall(shareDialog.present());
-        } else if (hasPublishPermission()) {
-            Request request = Request.newUploadPhotoRequest(Session.getActiveSession(), image, new Request.Callback() {
-                @Override
-                public void onCompleted(Response response) {
-                    //showPublishResult(getString(R.string.photo_post), response.getGraphObject(), response.getError());
+                    }
                 }
-            });
-            request.executeAsync();
-        } else {
-            pendingAction = PendingAction.POST_PHOTO;
-        }
-    }*/
-
-    
-    private void setFriendPickerListeners(final FriendPickerFragment fragment) {
-        fragment.setOnDoneButtonClickedListener(new FriendPickerFragment.OnDoneButtonClickedListener() {
-            @Override
-            public void onDoneButtonClicked(PickerFragment<?> pickerFragment) {
-                onFriendPickerDone(fragment);
-            }
-        });
-    }
-
-    private void onFriendPickerDone(FriendPickerFragment fragment) {
-        FragmentManager fm = getSupportFragmentManager();
-        fm.popBackStack();
-
-        String results = "";
-
-        List<GraphUser> selection = fragment.getSelection();
-        tags = selection;
-        if (selection != null && selection.size() > 0) {
-            ArrayList<String> names = new ArrayList<String>();
-            for (GraphUser user : selection) {
-                names.add(user.getName());
-            }
-            results = TextUtils.join(", ", names);
-        } else {
-            //results = getString(R.string.no_friends_selected);
-        }
-
-        //showAlert(getString(R.string.you_picked), results);
-    }
-
-    private void onPlacePickerDone(PlacePickerFragment fragment) {
-        FragmentManager fm = getSupportFragmentManager();
-        fm.popBackStack();
-
-        String result = "";
-
-        GraphPlace selection = fragment.getSelection();
-        if (selection != null) {
-            result = selection.getName();
-        } else {
-            //result = getString(R.string.no_place_selected);
-        }
-
-        place = selection;
-
-        //showAlert(getString(R.string.you_picked), result);
-    }
-
-    
-    private void setPlacePickerListeners(final PlacePickerFragment fragment) {
-        fragment.setOnDoneButtonClickedListener(new PlacePickerFragment.OnDoneButtonClickedListener() {
-            @Override
-            public void onDoneButtonClicked(PickerFragment<?> pickerFragment) {
-                onPlacePickerDone(fragment);
-            }
-        });
-        fragment.setOnSelectionChangedListener(new PlacePickerFragment.OnSelectionChangedListener() {
-            @Override
-            public void onSelectionChanged(PickerFragment<?> pickerFragment) {
-                if (fragment.getSelection() != null) {
-                    onPlacePickerDone(fragment);
+                if (response.getError() != null) {
+                    Log.d("SAIC","EL Error ES : "+ response.getRawResponse());
                 }
             }
         });
+        request.executeAsync();
     }
-
-    
-
-    private boolean hasPublishPermission() {
-        Session session = Session.getActiveSession();
-        return session != null && session.getPermissions().contains("publish_actions");
-    }
-
-    private void performPublish(PendingAction action, boolean allowNoSession) {
-        Session session = Session.getActiveSession();
-        if (session != null) {
-            pendingAction = action;
-            if (hasPublishPermission()) {
-                // We can do the action right away.
-                handlePendingAction();
-                return;
-            } else if (session.isOpened()) {
-                // We need to get new permissions, then complete the action when we get called back.
-                session.requestNewPublishPermissions(new Session.NewPermissionsRequest(this, PERMISSION));
-                return;
-            }
-        }
-
-        if (allowNoSession) {
-            pendingAction = action;
-            handlePendingAction();
-        }
-    }
-	
-	//no modificar, cosas de la actionBar
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	
 
 }
---
+++ app/src/main/java/mx/unam/saic/puntoycoma/util/ConnectionDetector.java
@@ -0,0 +1,33 @@
+package mx.yobibytelabs.rescatapp.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by jagspage2013 on 21/07/14.
+ */
+public class ConnectionDetector {
+
+    private Context _context;
+
+    public ConnectionDetector(Context _context){
+        this._context = _context;
+    }
+
+    public boolean isConnectedToInternet(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager !=null){
+            NetworkInfo[] networkInfo = connectivityManager.getAllNetworkInfo();
+
+            if(networkInfo!=null){
+                for(int i = 0; i<networkInfo.length;i++){
+                    if(networkInfo[i].getState() == NetworkInfo.State.CONNECTED ){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
